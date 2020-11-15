7daed2b0d730823b56d6715d9eadc9c54b28208e
digthedill <popssly@gmail.com>
2020-11-15 13:17:48 -0600

    gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,8 +12,8 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/content/galleries`,
-        name: `images`,
+        path: `${__dirname}/src`,
+        name: `src`,
       },
     },
     `gatsby-plugin-sharp`,

73e0ee1f5168bd4bbe39661ad2d117b37f314fa8
digthedill <popssly@gmail.com>
2020-11-18 00:49:22 -0600

    window compatibility with webpack

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react"
+import { window } from "browser-monads"
 import { Link } from "gatsby"
 import { FaInstagram, FaLinkedinIn } from "react-icons/fa"
 

4f8bc7b21ca8f8c54f5b86a639a4409f4cf5e009
digthedill <popssly@gmail.com>
2020-12-06 00:28:25 -0600

    added infinite-scroll

--- src/pages/photos.js
+++ src/pages/photos.js
@@ -3,6 +3,7 @@ import { graphql, useStaticQuery } from "gatsby"
 import Image from "gatsby-image"
 import { SRLWrapper } from "simple-react-lightbox"
 import Masonry from "react-masonry-css"
+import InfiniteScroll from "react-infinite-scroll-component"
 
 import { shuffleArray } from "../utils/randomizer"
 import Head from "../components/head"
@@ -64,23 +65,25 @@ const PhotosPage = () => {
       <Head title="Photos" />
       <h1>Photography</h1>
       <SRLWrapper options={options}>
-        <Masonry
-          breakpointCols={breakpointColumnsObj}
-          className="my-masonry-grid"
-          columnClassName="my-masonry-grid_column"
-        >
-          {shuffled.map((edge, i) => {
-            return (
-              <a href={edge.node.photo.url} key={i}>
-                <Image
-                  fluid={edge.node.photo.childImageSharp.fluid}
-                  className="image"
-                  alt={edge.node.title}
-                />
-              </a>
-            )
-          })}
-        </Masonry>
+        <InfiniteScroll dataLength={photos.length} loader={<h4>Loading...</h4>}>
+          <Masonry
+            breakpointCols={breakpointColumnsObj}
+            className="my-masonry-grid"
+            columnClassName="my-masonry-grid_column"
+          >
+            {shuffled.map((edge, i) => {
+              return (
+                <a href={edge.node.photo.url} key={i}>
+                  <Image
+                    fluid={edge.node.photo.childImageSharp.fluid}
+                    className="image"
+                    alt={edge.node.title}
+                  />
+                </a>
+              )
+            })}
+          </Masonry>
+        </InfiniteScroll>
       </SRLWrapper>
     </Layout>
   )

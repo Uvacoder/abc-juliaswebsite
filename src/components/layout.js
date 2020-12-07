fd88f41387341ce09536c6f5a649a098ec5253b7
digthedill <popssly@gmail.com>
2020-12-07 10:58:02 -0600

    testing speed w/o lightbox

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,5 @@
 import React from "react"
-import SimpleReactLightbox from "simple-react-lightbox"
+// import SimpleReactLightbox from "simple-react-lightbox"
 
 import Navbar from "./navbar"
 import Footer from "./footer"
@@ -9,15 +9,15 @@ import layoutStyle from "./layout.module.css"
 
 const Layout = props => {
   return (
-    <SimpleReactLightbox>
-      <div className={layoutStyle.body}>
-        <Navbar />
-        <div className={layoutStyle.content}>
-          <main>{props.children}</main>
-        </div>
-        <Footer />
+    // <SimpleReactLightbox>
+    <div className={layoutStyle.body}>
+      <Navbar />
+      <div className={layoutStyle.content}>
+        <main>{props.children}</main>
       </div>
-    </SimpleReactLightbox>
+      <Footer />
+    </div>
+    // </SimpleReactLightbox>
   )
 }
 
--- src/pages/photos.js
+++ src/pages/photos.js
@@ -1,9 +1,8 @@
 import React from "react"
 import { graphql, useStaticQuery } from "gatsby"
 import Image from "gatsby-image"
-import { SRLWrapper } from "simple-react-lightbox"
+
 import Masonry from "react-masonry-css"
-// import InfiniteScroll from "react-infinite-scroll-component"
 
 import { shuffleArray } from "../utils/randomizer"
 import Head from "../components/head"
@@ -11,25 +10,6 @@ import Layout from "../components/layout"
 
 import "../styles/masonry.css"
 
-const options = {
-  settings: {
-    overlayColor: "rgba(0, 0, 0, 0.9)",
-    height: "90vh",
-  },
-  buttons: {
-    showDownloadButton: false,
-    showThumbnailsButton: false,
-    showAutoplayButton: false,
-    size: "10px",
-    showCloseButton: true,
-  },
-  thumbnails: {
-    showThumbnails: false,
-  },
-  caption: {
-    showCaption: false,
-  },
-}
 const breakpointColumnsObj = {
   default: 3,
   1100: 3,
@@ -65,26 +45,24 @@ const PhotosPage = () => {
     <Layout>
       <Head title="Photos" />
       <h1>Photography</h1>
-      <SRLWrapper options={options}>
-        <Masonry
-          breakpointCols={breakpointColumnsObj}
-          className="my-masonry-grid"
-          columnClassName="my-masonry-grid_column"
-        >
-          {shuffled.map((edge, i) => {
-            return (
-              <div className="individualPhoto" key={i}>
-                <Image
-                  fluid={edge.node.photo.childImageSharp.fluid}
-                  className="image"
-                  alt={edge.node.title}
-                  style={{ cursor: "pointer" }}
-                />
-              </div>
-            )
-          })}
-        </Masonry>
-      </SRLWrapper>
+      <Masonry
+        breakpointCols={breakpointColumnsObj}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column"
+      >
+        {shuffled.map((edge, i) => {
+          return (
+            <div className="individualPhoto" key={i}>
+              <Image
+                fluid={edge.node.photo.childImageSharp.fluid}
+                className="image"
+                alt={edge.node.title}
+                style={{ cursor: "pointer" }}
+              />
+            </div>
+          )
+        })}
+      </Masonry>
     </Layout>
   )
 }
